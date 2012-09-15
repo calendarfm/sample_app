a18c9c035dc63b10a448d8ca8fc37baa2bb0518e
Justin Haight <justin@calendar.fm>
2012-09-14 19:52:55 -0700

    finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+  match '/signup', :to => 'users#new'
+  
   #You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => "pages#home"
@@ -6,6 +9,8 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
+  
+
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
